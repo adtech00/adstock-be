1488e17d46e469981ef0395735c7615d58f12bff
Abhit <abhitk@obsessory.com>
2020-02-01 18:28:14 +0530

    Revert "Revert "init""
    This reverts commit a063d3f57333c7c53de400a002974668205b8500.

---
+++ src/main/java/com/adtech/adstockbe/model/TestRes.java
@@ -0,0 +1,15 @@
+package com.adtech.adstockbe.model;
+
+import java.util.List;
+
+public class TestRes {
+    private List<String> res;
+
+    public List<String> getRes() {
+        return res;
+    }
+
+    public void setRes(List<String> res) {
+        this.res = res;
+    }
+}
---
+++ src/test/java/com/adtech/adstockbe/AdstockBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adtech.adstockbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdstockBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
