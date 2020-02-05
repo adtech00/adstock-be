1896207b5c0f56659b800dd97730faf2c652c3b9
AD TECH <54678307+admin-adtech@users.noreply.github.com>
2020-02-05 15:14:28 +0530

    server.sh file

---
+++ restartserver.sh
@@ -0,0 +1,6 @@
+app_id=$(ps -ef | grep 'adstock-be-0.0.1-SNAPSHOT.jar' | grep -v 'grep' | awk '{ printf $2 }')
+sudo kill $app_id
+echo Killed AD_STOCK_BE ..........
+echo Restarting..................
+echo --------------------------------------------------------------------------------------------------------------
+nohup java -jar "target/adstock-be-0.0.1-SNAPSHOT.jar" &
